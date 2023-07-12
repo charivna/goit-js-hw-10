1a4f6312f8808e3a8ad21c818b10847cb5309dfd
charivna <124678194+charivna@users.noreply.github.com>
2023-07-12 20:48:42 +0200

    puuuush

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,52 +1,33 @@
-import axios from "axios";
+// import axios from "axios";
+// axios.defaults.headers.common["x-api-key"] = "live_hlBvwU7f4qPZLTDeNehIKrPTknEulvSeQPwDAYbbrTXj4DszHTLcadwiaj0rwN8g"; - НЕ ПРАЦЮЄ!
 
-axios.defaults.headers.common["x-api-key"] = "live_hlBvwU7f4qPZLTDeNehIKrPTknEulvSeQPwDAYbbrTXj4DszHTLcadwiaj0rwN8g";
 
-// колекція порід
 
-export const BASE_URL = 'https://api.thecatapi.com/v1'
+
+const BASE_URL = 'https://api.thecatapi.com/v1';
+const API_KEY = 'live_hlBvwU7f4qPZLTDeNehIKrPTknEulvSeQPwDAYbbrTXj4DszHTLcadwiaj0rwN8g'
+
 
 export function fetchBreeds() {
-     fetch(`${BASE_URL}/breeds`)
+
+    return fetch(`${BASE_URL}/breeds`)
         .then(response => {
             if (!response.ok) {
                 throw new Error(response.statusText)
             }
-        return response.json();
+            return response.json();
         })
-        .then((data) => {
-            const catInfo = data
-                .map(({ id, name }) => `<option value="${id}">${name}</option>`)
-                .join(' ');
-            
-            refs.selectEl.insertAdjacentHTML('beforeend', catInfo);
-        
-        }
-        )
-        .catch(err => {
-        console.log(err);
-    })
 }
 
-export function fetchCatByBreed(breedId) {
-   fetch(`${BASE_URL}/images/search?breed_ids=${breedId}`)
+export function fetchCatByBreed(breedId)
+{
+    return fetch(`${BASE_URL}/images/search?breed_ids=${breedId}&api_key=${API_KEY}`)
         .then(response => {
             if (!response.ok) {
-                throw new Error(response.statusText)
+             throw new Error(response.statusText)
             }
             return response.json()
-        })
-        .then((data) => {
-            if (data.length > 0) {
-                const catData = data[0];
-                const catImage = `<img srs="${catData.url}" alt="Cat image"></img>`;
-                const catInfo = `<h2>${catData.breeds[0].name}</h2>
-          <p>Description: ${catData.breeds[0].description}</p>
-          <p>Temperament: ${catData.breeds[0].temperament}</p>
-        `;
-                refs.infoCat.innerHTML = catImage + catInfo;
-            }
-            
-        }
-        )
+    })
 }
+         
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
 import { fetchBreeds, fetchCatByBreed } from "./cat-api"
 
-refs = {
+const refs = {
     selectEl: document.querySelector('select.breed-select'),
     infoCat: document.querySelector('.cat-info'),
     errorEl: document.querySelector('.error'),
     loaderEL: document.querySelector('.loader')
 }
 
+fetchBreeds().then(data => {
+    getValueToSelect(data)
+}).catch(err => console.log(err))
+    
 
-fetchBreeds()
+function getValueToSelect(data) {
+   const catsInfo = data
+    .map(({ id, name }) => `<option value="${id}">${name}</option>`)
+    .join(' ');  
+    refs.selectEl.insertAdjacentHTML('beforeend', catsInfo);
+    } 
 
-refs.selectEl.addEventListener('change', (event) => {
-    const selectedBreedId = event.target.value;
+refs.selectEl.addEventListener('change', onSelect)
 
-    fetchCatByBreed(selectedBreedId);
-});
+function onSelect(event) {
+    fetchCatByBreed(event.target.value)
+        .then(data => {
+            let catInfo = data[0];
+            console.log(catInfo);
+            const catCard = `<img src="${catInfo.url}" alt="Cat image" width = 500></img>
+            <h2>${catInfo.breeds[0].name}</h2>
+            <p>Description: ${catInfo.breeds[0].description}</p>
+            <p>Temperament: ${catInfo.breeds[0].temperament}</p>`
+
+            refs.infoCat.innerHTML = catCard
+        }).catch( err => console.log( err ) )
+}
+
+function hideLoader() {
+    refs.loaderEL.style.display = 'none'
+}
+
+function showLoader() {
+    refs.loaderEL.style.display = 'block'
+}
+
+function hideSelect() {
+    refs.selectEl.style.display = 'none'
+}
+
+function showSelect() {
+    refs.selectEl.style.display = 'block'
+}
